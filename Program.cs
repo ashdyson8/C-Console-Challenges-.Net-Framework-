356ab79037d1a82c444b937e6d39785945e24edf
ashdyson8 <59566987+ashdyson8@users.noreply.github.com>
2020-01-10 12:04:15 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 3, 4, -8,4,5, 6,2, 9, 1, -11, 15 };
+
+            int n = arr.Length;
+
+            Console.WriteLine(findMissingPositive(arr, n));
+            Console.ReadKey();
+        }
+        static int findMissingPositive(int[] arr, int n)
+        {
+            int m = 1;
+
+            HashSet<int> x = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+             
+                if (m < arr[i])
+                {
+                    x.Add(arr[i]);
+                }
+
+                else if (m == arr[i])
+                {
+                   
+                    m = m + 1;
+
+                    while (x.Contains(m))
+                    {
+                        x.Remove(m);
+
+                        
+                        m = m + 1;
+                    }
+                }
+            }
+            return m;
+        }
+        
+    }
+}
