be4f44ab1370bf5bf1b39e4290891b9b5b2ebe2a
ashdyson8 <59566987+ashdyson8@users.noreply.github.com>
2020-01-21 20:27:49 +0000

    Recursive function

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            double factorial = Factorial(number);
+            Console.WriteLine("factorial of " + number.ToString() + "=" + factorial.ToString());
+            Console.ReadLine();
+
+        }
+        public static double Factorial (int number)
+        {
+            if (number == 0)
+                return 1;
+
+            return number * Factorial(number - 1);
+        }
+
+    }
+}
